9dd23c156f25bffda9e77907e5f62bef2f3ff1eb
NamanNair18 <namannair18@gmail.com>
2025-08-28 21:51:47 +0530

    Create Day1-8.py

---
+++ Day1/Day1-8.py
@@ -0,0 +1,7 @@
+radius = int(input("Enter The Radius Of the Circle:- "))
+
+Area = 3.14*radius*radius
+print(f"Radius:- {Area}")
+
+circumference = 2*3.14*radius
+print(f"Circumference:- {circumference}") 
