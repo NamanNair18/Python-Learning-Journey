913493718679e9518fc4ceb9fc723a9d4bb393d0
NamanNair18 <namannair18@gmail.com>
2025-08-28 21:51:39 +0530

    Create Day1-5.py

---
+++ Day1/Day1-5.py
@@ -0,0 +1,8 @@
+#take two numbers from user and print their sum, difference, product, and quotient.
+a = int(input("Enter the Value of a:-"))
+b = int(input("Enter the Value of b:-"))
+
+print(f"sum:-{a+b}")
+print(f"Difference:-{a-b}")
+print(f"Product:-{a*b}")
+print(f"Quotient:-{a%b}")
