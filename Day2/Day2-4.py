05b6c3e27c7425f419bcd103257a7e48e1d46828
NamanNair18 <namannair18@gmail.com>
2025-08-29 23:04:34 +0530

    Create Day2-4.py

---
+++ Day2/Day2-4.py
@@ -0,0 +1,5 @@
+#Count how many times a given element occurs in a list.
+lst = [1,2,3,2,4,3,5,2,4,3,6,1,4,2,4,6]
+i = int(input("Enter The number:- "))
+count = lst.count(i)
+print(f"count:={count}")
