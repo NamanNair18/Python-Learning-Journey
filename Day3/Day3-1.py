34fae9a16d5b1f0380b2bad3b3e3a16b3f9e2248
NamanNair18 <namannair18@gmail.com>
2025-08-30 23:22:51 +0530

    Create Day3-1.py

---
+++ Day3/Day3-1.py
@@ -0,0 +1,9 @@
+#Write a program to check if a number is positive, negative, or zero.
+num = int(input("Enter The Number"))
+
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else :
+    print("It's Zero")
