b6f7e3a3d61759f8787fad5282242f187e1214ce
NamanNair18 <namannair18@gmail.com>
2025-09-08 11:40:22 +0530

    Create Day5-4.py

---
+++ Day5/Day5-4.py
@@ -0,0 +1,9 @@
+#Write a function to check whether a number is prime or not.
+
+def Check_num(num):
+    if num <= 1:
+        return False
+    for i in range (2, int(num**0.5)+1):
+        if num % i == 0 :
+            return False
+    return True
