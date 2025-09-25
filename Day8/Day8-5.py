e21536030237dc09bd04e2c5dfd64a2dfe7f140f
NamanNair18 <namannair18@gmail.com>
2025-09-25 11:47:22 +0530

    Create Day8-5.py

---
+++ Day8/Day8-5.py
@@ -0,0 +1,17 @@
+#Show an example of operator overloading (+ operator to add two objects).
+class Addition:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Addition(self.x + other.x , self.y + other.y)
+    
+    def __repr__(self):
+        return f"Addition({self.x}, {self.y})"
+
+v1 = Addition(2, 3)
+v2 = Addition(4, 5)
+v3 = v1 + v2
+
+print(v3)
